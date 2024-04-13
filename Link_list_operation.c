4d478b0b56e4b9df5a03fc9e543d026f54c4f201
System_32 <162736264+nishantThosar@users.noreply.github.com>
2024-04-13 19:05:29 +0530

    Link_list_operation.c
    Updated by CDAC PC

--- Link_list_operation.c
+++ Link_list_operation.c
@@ -10,6 +10,9 @@ struct link_list {//declration of strcut .
 void add_beg(void);
 void display(void);
 void first_node(void);
+void del_at_end(void);
+void free_ptr(void);
+
 struct link_list* make_node(void);
 
 struct link_list *head, *t1; 
@@ -22,28 +25,44 @@ int main()
 	while(1)
 	{
 		printf("Enter Numerical value: ");
-		printf("\n1: add node at begning");
-		printf("\n2: add node at begning");
-		printf("\n3: display all node");
-		printf("\n0: exit\t");
+		printf("\n1: Add first node");
+		printf("\n2: Add node at begning");
+		printf("\n3: Display all node");
+		printf("\n4: Delete at end");
+		printf("\n5: To delete whole list");
+		printf("\n0: Exit\t");
+		printf("\n");
+		printf("\nEnter choice:\t");
+		
 		scanf("%d",&choice);
 	
 		//Switch case for menu driven program	
 		switch(choice){
 			
 			case 0:	printf("\nThankyou\n");
+				printf("\nCurrent Nodes: %d", total_node);
 				exit (0);
 				break;
 	
 			case 1:first_node();
+				printf("\nCurrent Nodes: %d", total_node);
 				break;
 			
 			case 2:add_beg();
+				printf("\nCurrent Nodes: %d", total_node);
 				break;
 			
 			case 3: display();
+				printf("\nCurrent Nodes: %d", total_node);
 				break;	
-				
+			
+			case 4:del_at_end();
+				printf("\nCurrent Nodes: %d", total_node);
+			       break;
+
+			case 5: free_ptr();
+				printf("\nDeleted all list");
+				break;
 		}
 	}
 	return 0;
@@ -54,17 +73,16 @@ void first_node()
 {
 
 	if (head==NULL)//to check weither the link list is preasnt  or not.
-	{
-		printf("\nthere is no link list");
-	
+	{	
 		struct link_list *temp;
 		temp =(struct link_list *)malloc(sizeof( struct link_list));
 		//creating first node.
-		printf("Enter the data in first element: ");
+		printf("\nEnter the data in first element: ");
 		scanf("%d",&temp->data);
 		temp->next=NULL;
 		head=temp;
 		++total_node;// to keep check of no of nodes in the list.
+		
 	}
 }
 
@@ -76,7 +94,7 @@ void add_beg(){//function calling
 	struct link_list *temp;
         
 	if(head==NULL){
-		printf("There is no link list");
+		printf("\nThere is no link list");
 		printf("\nAdd first node to the list.");
 		temp = (struct link_list*)malloc(sizeof(struct link_list));
 		head = temp;
@@ -117,16 +135,42 @@ struct link_list*  make_node()
 }
 
 
+//fuction for delete node form end
+void del_at_end()
+{
+
+	struct link_list *t2;
+	t2=head;
+	t1=head;
+	while(t1->next!=NULL)
+	{
+		t2=t1;
+		t1 = t1->next;
+	}
+	free(t1);
+	t2 -> next = NULL;
+	--total_node;
+}//delete at end function ends.
 
 
+//To avoid any memory leak.
+void free_ptr()
+{
+	
+	struct link_list *t2;
+	struct link_list *t1;
+	t1 = t2 = head;
+	int count =0;
 
+	while(t1->next != NULL){
+		++count;
+		t2 = t1;
+		t1 = t1->next;
+	}//while ends
+	t2->next = NULL;
+	free(t1);
+//	free(head);
+//	free(ptr);
 
-
-
-
-
-
-
-
-
-
+	printf("Total nodes freed: %d", count);
+}//Free ptr ends
